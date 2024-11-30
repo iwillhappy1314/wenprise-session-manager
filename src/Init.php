b00fec8f345fe77705aed237a950a23a83fb6cff
Amos Lee <iwillhappy1314@gmail.com>
2024-11-30 21:36:01 +0800

    初始化，实现基于数据库和 object 的 cache

---
+++ src/Init.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Wenprise\SessionManager;
+
+use Wenprise\SessionManager\Handlers\DatabaseHandler;
+
+class Init {
+
+	public function __construct() {
+		if ( ! isset( $_SESSION ) ) {
+			// Queue up the session stack.
+			$wp_session_handler = Manager::initialize();
+			$wp_session_handler->addHandler( new DatabaseHandler() );
+
+			/**
+			 * The database handler can automatically clean up sessions as it goes. By default,
+			 * we'll run the cleanup routine every hour to catch any stale sessions that PHP's
+			 * garbage collector happens to miss. This timeout can be filtered to increase or
+			 * decrease the frequency of the manual purge.
+			 *
+			 * @param string $timeout Interval with which to purge stale sessions
+			 */
+			$timeout = apply_filters( 'wenprise_session_gc_interval', 'hourly' );
+
+			if ( ! wp_next_scheduled( 'wenprise_session_database_gc' ) ) {
+				wp_schedule_event( time(), $timeout, 'wenprise_session_database_gc' );
+			}
+
+			add_action( 'wenprise_session_database_gc', [ 'EAMann\WPSession\DatabaseHandler', 'directClean' ] );
+
+			$_SESSION[ 'wenprise_session_manager' ] = 'active';
+		}
+
+		// If we're not in a cron, start the session
+		if ( ! defined( 'DOING_CRON' ) || false === DOING_CRON ) {
+			if ( session_status() !== PHP_SESSION_ACTIVE ) {
+				session_start();
+			}
+		}
+
+	}
+
+}
